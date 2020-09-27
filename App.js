feade70d7b3116a243ec4fc1a72c79a04a9cf05d
deeps120 <62687789+deeps120@users.noreply.github.com>
2020-09-27 17:22:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { createAppContainer, createSwitchNavigator,} from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import ExchangeScreen from './screens/ExchangeScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import HomeScreen from './screens/HomeScreen';
+import { AppDrawerNavigator } from './component/AppDrawerNavigator';
+import CustomSideBarMenu from './componenets/CustomSideBarMenu';
+import MyBartersScreen from './screens/MyBartersScreen';
+
+export default function App() {
+  return (
+    <AppContainer/>
+  );
+}
+
+
+const switchNavigator = createSwitchNavigator({
+  WelcomeScreen:{screen: WelcomeScreen},
+  Home:{screen: HomeScreen},
+  Exchange:{screen: ExchangeScreen},
+  barters :{screen : MyBartersScreen}
+})
+
+const AppContainer =  createDrawerNavigator({
+home:{
+  screen: TabNavigator
+},
+
+settings:{
+  screen:SettingsScreen
+}
+},
+
+{
+  contentComponent : CustomSideBarMenu
+},
+{
+  initialRouteName:'Home'
+
+})
+
+
+
+
